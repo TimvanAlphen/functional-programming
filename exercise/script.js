732d36ecd1983771a5fc4c8d45e735a3aa2b13f8
TimvanAlphen <55843527+TimvanAlphen@users.noreply.github.com>
2020-10-28 17:14:32 +0100

    Live code Laurens

--- exercise/script.js
+++ exercise/script.js
@@ -1,1 +1,25 @@
 console.log("Hello world")
+
+const endpoint = 'https://opendata.rdw.nl/resource/t5pc-eb34.json'
+const selectedColumn = 'areaid'
+
+getData(endpoint)
+  .then(data => {
+  	console.log("all data: ", data)
+  	console.log("one datum,", data[0])
+	const areaIdArray = filterData(data, selectedColumn)
+	const usageArray = filterData(data, 'usageid')
+	console.log(usageArray)
+})
+
+async function getData(url){
+	const response = await fetch(url)
+	const data = await response.json()
+	return data
+}
+
+
+
+function filterData(dataArray, column) {
+	return dataArray.map(item => item[column])
+}
